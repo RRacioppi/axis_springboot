459fc398938ced61c9e7626346d609f6fa3c649a
Racioppi Renzo <rracioppi@imolainformatica.it>
2019-12-10 12:36:20 +0100

    initial commit

---
+++ src/main/java/com/example/axisws/AxiswsApplication.java
@@ -0,0 +1,13 @@
+package com.example.axisws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxiswsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxiswsApplication.class, args);
+	}
+
+}
